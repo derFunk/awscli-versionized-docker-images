75ceb366b9d15197dc5c98bfe19d326493d58109
Andreas Katzig <andreas.katzig@ottonova.de>
2017-09-12 15:18:50 +0200

    - Deleting built images to save local disk space - Make fetching available aws cli versions more robust

--- build_and_push_all.sh
+++ build_and_push_all.sh
@@ -7,7 +7,7 @@ REPO=derfunk/awscli-versionized
 i="1"
 echo -n > awscli-versioned-versions.txt
 while true; do
-	echo "Fetching page ${i} of existing versionized aws cli tags..."
+	echo "Fetching page ${i} of already existing versionized aws cli tags..."
 	if ! curl -fs -o awscli-versioned-tmp.json https://hub.docker.com/v2/repositories/${REPO}/tags/\?page_size\=100\&page\=${i}; then
 		echo "Page ${i} did not exist anymore, continuing..."
 		break;
@@ -17,10 +17,13 @@ while true; do
 done
 rm -f awscli-versioned-tmp.json
 
+# Get current .changes tree SHA
+JSON_MASTER=$(curl -fs https://api.github.com/repos/aws/aws-cli/git/trees/master)
+CHANGES_JSON_URL=$(echo ${JSON_MASTER} | jq -r '.tree[] | select(.path==".changes" and .type=="tree") | .url')
+
 # Get all official aws cli versions 
-curl -fs -o awscli-changes.json https://api.github.com/repos/aws/aws-cli/git/trees/fb9089827fea2fe4d79daf8063380913e2aa7dbf
-jq -r ".tree | .[].path" awscli-changes.json | grep "\d\+.*\.json" | sed "s/\.json//g" > awscli-versions.txt
-rm -f awscli-changes.json
+JSON_CHANGES=$(curl -fs ${CHANGES_JSON_URL})
+echo ${JSON_CHANGES} | jq -r ".tree | .[].path" | grep "\d\+.*\.json" | sed "s/\.json//g" > awscli-versions.txt
 
 cat awscli-versions.txt | while read version
 do
@@ -28,7 +31,7 @@ do
 	if [ "${PUSH_EXISTING}" = "true" ] || ! grep -q "^${version}$" awscli-versioned-versions.txt; then
 		echo "Building and pushing aws-cli version ${version}..."
     	docker build --build-arg AWSCLI_VERSION=${version} -t ${REPO}:${version} .
-    	docker push ${REPO}:${version}
+    	docker push ${REPO}:${version} && docker rmi ${REPO}:${version}
     else
     	echo "Not pushing aws-cli version ${version} because it's already present."
     fi
